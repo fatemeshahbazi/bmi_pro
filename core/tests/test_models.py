fee369eff2afb8c65451dd37a38be29b400ebfd6
fa-shahbazi <fa.shahbazi2021@gmail.com>
2021-12-20 22:42:38 +0330

    update test_mdels

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -4,10 +4,15 @@ from core.models import BmiMeasurement
 
 class TestModels(APITestCase):
     def test_creates_BmiMeasurement(self):
-        bmi1 = BmiMeasurement.objects.create(height=1.65, weight=70, bmi=25.71, result='OVERWEIGHT')
+        bmi1 = BmiMeasurement.objects.create(height=1.65, weight=70)
         self.assertIsInstance(bmi1, BmiMeasurement)
         self.assertEqual(bmi1.height, 1.65)
-        self.assertEqual(bmi1.bmi, 25.71)
+        self.assertEqual(bmi1.bmi, 25.71166207529844)
         self.assertEqual(bmi1.weight, 70)
-        self.assertEqual(bmi1.result, 'OVERWEIGHT')
+        self.assertEqual(bmi1.result, 'overweight')
 
+    def test_get_bmi(self):
+        self.assertEqual(BmiMeasurement.get_bmi(65, 1.65), 23.875114784205696)
+
+    def test_get_result(self):
+        self.assertEqual(BmiMeasurement.get_result(23), BmiMeasurement.NORMAL)
